c2eead098ad0d65040211fda30b1e6cc11595523
Matheus Levy <srlevy008@gmail.com>
2024-02-18 14:48:53 -0300

    Update scriptTest.cs

--- estudos/Assets/Scripts/scriptTest.cs
+++ estudos/Assets/Scripts/scriptTest.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class scriptTest : MonoBehaviour
 {
+    #region VARIAVEIS
+    private int doors = 4;
+    public float life = 10;
+    public float damege = 0.5f;
+    protected bool acelerate = false;
+    public GameObject myGameObject;
+    private Transform myTransform;
+    protected Color color = Color.red;
+    #endregion
+    
+    #region METODOS
     void Start()
     {
         
@@ -13,4 +24,7 @@ public class scriptTest : MonoBehaviour
     {
         
     }
+    #endregion
+
+
 }
