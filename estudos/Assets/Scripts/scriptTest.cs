11d8a55c5c927674e07066e9046300ec04a563ee
Matheus Levy <srlevy008@gmail.com>
2024-02-26 20:46:05 -0300

    1.7

--- estudos/Assets/Scripts/scriptTest.cs
+++ estudos/Assets/Scripts/scriptTest.cs
@@ -5,15 +5,15 @@ using MeuJogo.Bus.Maneger;
 
 public class scriptTest : MonoBehaviour
 {
+    public string minhaString;
     public bool checkStatus= false;
-
     [Range(-5,5)]
     public int value01;
     public int value02;
 
     private void Update()
     {
-       CheckValue();
+       //CheckValue();
        CheckSwitchCase();
     }
     private void CheckValue()
@@ -33,6 +33,23 @@ public class scriptTest : MonoBehaviour
     }
     private void CheckSwitchCase()
     {
+        switch(minhaString)
+        {
+            case "Levy":
+                Debug.Log("Humano");
+            break;
+            case "Batatinha":
+                Debug.Log("Gata");
+            break;
+            case "Mel":
+                Debug.Log("Cadela");
+            break;
+            default:
+                Debug.Log("ESSE SER EU N CONHEÇO AINDA");
+            break;
 
+        }
     }
+
+
 }
